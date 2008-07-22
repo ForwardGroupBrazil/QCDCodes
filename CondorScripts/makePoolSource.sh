40dac5739adb9a824ac16e37e2f6a767d6ac869a
aeverett <>
2008-07-22 19:07:52 +0000

    *** empty log message ***

--- CondorScripts/makePoolSource.sh
+++ CondorScripts/makePoolSource.sh
@@ -6,7 +6,6 @@ castorDir2=$2
 echo ${castorDir}
 echo ${castorDir2}
 
-#read -r -a FileList <<< $(rfdir /castor/cern.ch/user/a/aeverett/note-reco209-01/minusPt1000)
 read -r -a FileList <<< $(rfdir ${castorDir}/${castorDir2})
 
 element_count=${#FileList[@]}
@@ -14,37 +13,29 @@ let "end_count = $element_count - 1"
 
 index=8
 
-#   for i in "${FileList[@]}"
-#   do
-#     echo "$i"
-#   done
-
 cat << EOF >> ${castorDir2}.txt
 replace PoolSource.fileNames = {
 EOF
-
+match="edm"
+first=1
 while [ "$index" -lt "$element_count" ]
   do    # List all the elements in the array.
-  if [ "$index" -ne "$end_count" ]; then
-      cat << EOF >> ${castorDir2}.txt
-   'rfio:${castorDir}/${FileList[$index]}',
+#  if [ "$index" -ne "$end_count" ]; then
+      if [[ "${FileList[$index]}" =~ "${match}" ]]; then
+	  if [ $first -eq 1 ]; then
+	      let "first = 0"
+	      cat << EOF >> ${castorDir2}.txt
+   'rfio:${castorDir}/${castorDir2}/${FileList[$index]}'
 EOF
-  fi
-  if [ "$index" -eq "$end_count" ]; then
-      cat << EOF >> ${castorDir2}.txt
-  'rfio:${castorDir}/${FileList[$index]}'
+	  fi
+	  cat << EOF >> ${castorDir2}.txt
+   ,'rfio:${castorDir}/${castorDir2}/${FileList[$index]}'
 EOF
+#      fi
   fi
   let "index = $index + 9"
-  # Or:
-  #    index+=1
-  # if running Bash, version 3.1 or later.
 done
 
 cat << EOF >> ${castorDir2}.txt
 }
 EOF
-
-#echo ${FileList[17]}
-
-
