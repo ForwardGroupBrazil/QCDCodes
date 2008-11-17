0b01bf155db85a4aa42ec2ba4b3ca5f80b62890d
aeverett <>
2008-11-17 21:20:28 +0000

    transpose matrix

--- L3Switches/python/ptEtaRescale.py
+++ L3Switches/python/ptEtaRescale.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 ptRange = cms.vdouble(0 , 13, 30, 70, 1000)
 etaRange = cms.vdouble(0 , 1.0, 1.4, 10)
 
-diagTerm = cms.PSet( values = cms.vdouble( 3, 5, 10, 10,
-                                           3, 4,  7, 10,
-                                           3, 5, 10, 10
+diagTerm = cms.PSet( values = cms.vdouble( 3, 3, 3, 5,
+                                           4, 5,  10, 7,
+                                           10, 10, 10, 10
                                            ))
 offDiagTerm = cms.PSet( values = cms.vdouble( 1, 1, 1, 1,
                                               1, 1, 1, 1,
