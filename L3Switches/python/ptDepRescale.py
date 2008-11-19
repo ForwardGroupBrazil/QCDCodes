248d7b70e775e946d360abc0588f8745b69a342b
cliu <>
2008-11-19 04:16:23 +0000

    fix

--- L3Switches/python/ptDepRescale.py
+++ L3Switches/python/ptDepRescale.py
@@ -3,6 +3,10 @@ import FWCore.ParameterSet.Config as cms
 ptRange = cms.vdouble(0 , 13, 30, 1000)
 etaRange = cms.vdouble(0 ,10)
 
-diagTerm = cms.PSet( values = cms.vdouble( 3, 5, 10 ))
-offDiagTerm = cms.PSet( values = cms.vdouble( 1, 1, 1 ))
+diagTerm = cms.PSet( values = cms.vdouble( 3, 5, 10 ),
+action = cms.string("scale")
+)
+offDiagTerm = cms.PSet( values = cms.vdouble( 1, 1, 1 ),
+action = cms.string("scale")
+)
 
--- L3Switches/python/ptEtaRescale.py
+++ L3Switches/python/ptEtaRescale.py
@@ -6,9 +6,13 @@ etaRange = cms.vdouble(0 , 1.0, 1.4, 10)
 diagTerm = cms.PSet( values = cms.vdouble( 3, 3, 3, 5,
                                            4, 5,  10, 7,
                                            10, 10, 10, 10
-                                           ))
+                                           ),
+action = cms.string("scale")
+)
 offDiagTerm = cms.PSet( values = cms.vdouble( 1, 1, 1, 1,
                                               1, 1, 1, 1,
                                               1, 1, 1, 1
-                                              ))
+                                              ),
+action = cms.string("scale")
+)
 
