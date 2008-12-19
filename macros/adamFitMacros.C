099424755f33203da976a7103d0ab62a6e078130
aeverett <>
2008-12-19 20:49:31 +0000

    add a TH1 fit function

--- macros/adamFitMacros.C
+++ macros/adamFitMacros.C
@@ -130,3 +130,97 @@ TGraph* fit(const TH2* histo, int fitType = 2){
 
   return grW;
 }
+
+std::pair<double,double>
+fit(const TH1* histo, int fitType = 2)
+{
+  static int i = 0;
+  i++;
+
+  vector<double> width;
+  vector<double> mass;
+
+  // Errors
+  vector<double> widthError;
+  vector<double> massError;
+  
+  // Chi2
+  vector<double> chi2;
+  
+  
+  double cont_min = 1; 
+  Int_t binx =  histo->GetXaxis()->GetNbins();
+  
+  //histo->RebinY(4);
+  //histo->RebinX(2);
+  
+  double cont = histo->GetEntries();
+  
+  if (cont >= cont_min) {
+    
+    float minfit = histo->GetMean() - histo->GetRMS();
+    float maxfit = histo->GetMean() + histo->GetRMS();
+    
+    
+    TString iString(i);   
+    
+    
+    TF1 *fitFcn = new TF1(TString("g")+histo->GetName()+iString,"gaus",minfit,maxfit);
+    double x1,x2;
+    fitFcn->GetRange(x1,x2);
+    histo->Fit(fitFcn,"QR0","",x1,x2);
+    double *par = fitFcn->GetParameters();
+    double *err = fitFcn->GetParErrors();
+    
+    // Values
+    mass.push_back(par[1]);
+    width.push_back(par[2]);
+    
+    // Errors
+    massError.push_back(err[1]);
+    widthError.push_back(err[2]);
+    
+    chi2.push_back(fitFcn->GetChisquare());
+    
+    
+    if (fitFcn) delete fitFcn;
+  }
+  
+  const int nn= mass.size();
+  double *x = new double[nn];
+  double *ym = new double[nn];
+  double *e = new double[nn];
+  double *eym = new double[nn];
+  double *yw = new double[nn];
+  double *eyw = new double[nn];
+  double *yc = new double[nn];
+  
+  for (int j=0;j<nn;j++){
+    //x[j]=binCentre[j];
+    ym[j]=mass[j];
+    eym[j]=massError[j];
+    yw[j]=width[j];
+    eyw[j]=widthError[j];
+    
+    yc[j]=chi2[j];
+    e[j]=0.;
+    
+  }
+  
+  if(nn>1) cout << "ERROR in fit macro: nn > 1 nn=" << nn << endl;
+  
+  std::pair<double,double> returnVal;
+  if(fitType==1) {returnVal.first = ym[0]; returnVal.second = eym[0]; }
+  if(fitType==2) {returnVal.first = yw[0]; returnVal.second = eyw[0]; }
+  if(fitType==3) {returnVal.first = yc[0]; returnVal.seconsd = e[0]; }
+  
+  if(x) delete x;
+  if(ym) delete ym;
+  if(e) delete e;
+  if(eym) delete eym;
+  if(yw) delete yw;
+  if(eyw) delete eyw;
+  if(yc) delete yc;
+  
+  return returnVal;
+}
