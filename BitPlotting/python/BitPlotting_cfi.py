cf42ad2823f543e33ab1cfd5f04682e5796364d0
vlimant <>
2009-05-15 14:26:01 +0000

    a newer version

--- BitPlotting/python/BitPlotting_cfi.py
+++ BitPlotting/python/BitPlotting_cfi.py
@@ -1,14 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 # Bit Plotting
-bitSummary = cms.EDAnalyzer(
-    "BitPlotting",
+bitSummary = cms.EDAnalyzer("BitPlotting",
+     directory = cms.untracked.string('myPlots'),
+     label = cms.string('myLabel'),
      out = cms.untracked.string('dqm.root'),
      HLTPaths = cms.vstring('HLT_L1MuOpen','HLT_L1Mu','HLT_L2Mu9',
                            'HLT_IsoMu15','HLT_IsoMu11','HLT_IsoMu9',
                            'HLT_Mu15','HLT_Mu13','HLT_Mu11','HLT_Mu9',
                            'HLT_Mu7','HLT_Mu5','HLT_Mu3','HLT_DoubleMu3',
                            'HLT_DoubleIsoMu3'),
-     TriggerResultsTag = cms.InputTag('TriggerResults','','HLT2')
+    denominator = cms.untracked.string('HLT_L1MuOpen'),
+    
+    TriggerResultsTag = cms.InputTag('TriggerResults','','HLT2')
     )
 
--- BitPlotting/src/BitPlotting.cc
+++ BitPlotting/src/BitPlotting.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Jean-Roch Vlimant
 //         Created:  Thu Jul 17 12:57:41 CEST 2008
-// $Id: BitPlotting.cc,v 1.1 2008/11/07 14:58:32 aeverett Exp $
+// $Id: BitPlotting.cc,v 1.2 2008/11/13 15:38:15 aeverett Exp $
 //
 //
 
@@ -65,17 +65,21 @@ private:
 
   std::vector<std::string > HLTPathsByName_;
   std::vector<unsigned int> HLTPathsByIndex_;
+  std::string denominator_;
 
   std::vector<unsigned int> count_;
-  //  std::map<std::string, unsigned int> count_;
+
 
   unsigned int total_;
 
-  std::string out;
+  std::string out_;
 
+  std::string directory_;
+  std::string label_;
   MonitorElement * h1_;
   MonitorElement * h2_;
   MonitorElement * pf_;
+  MonitorElement * ratio_;
 
 };
 
@@ -97,7 +101,11 @@ BitPlotting::BitPlotting(const edm::ParameterSet& iConfig) :
 {
   count_.resize(HLTPathsByName_.size());
   HLTPathsByIndex_.resize(HLTPathsByName_.size());
-  out = iConfig.getUntrackedParameter<std::string>("out");
+  out_ = iConfig.getUntrackedParameter<std::string>("out","");
+
+  denominator_ = iConfig.getUntrackedParameter<std::string>("denominator");
+  directory_ = iConfig.getUntrackedParameter<std::string>("directory","BitPlotting/");
+  label_ = iConfig.getParameter<std::string>("label");
 }
 
 
@@ -119,21 +127,25 @@ void BitPlotting::beginRun(const edm::Run  & r, const edm::EventSetup  &){
   
   edm::LogError("BitPlotting")<<"this is the beginning of a NEW run: "<< r.run();
 
-  //  h1_ = edm::Service<TFileService>()->make<TH1F>(("passingBits_"+rN).c_str(),("passing bits in run "+rN).c_str(),nBin,min,max);
-  //  h2_ = edm::Service<TFileService>()->make<TH2F>(("passingBits_2_"+rN).c_str(),("correlation between bits in run "+rN).c_str(),nBin,min,max, nBin,min,max);
-  //  pf_ = edm::Service<TFileService>()->make<TProfile>(("fraction_"+rN).c_str(),("fraction of passing bits in run "+rN).c_str(),nBin,min,max, 0, 1);
+  edm::Service<DQMStore>()->setCurrentFolder(directory_);
 
-  edm::Service<DQMStore>()->setCurrentFolder("BitPlotting");
-
-  h1_ = edm::Service<DQMStore>()->book1D(std::string("passingBits_"+rN),std::string("passing bits in run "+rN),nBin,min,max);
-  h2_ = edm::Service<DQMStore>()->book2D(std::string("passingBits_2_"+rN),std::string("correlation between bits in run "+rN),nBin,min,max, nBin,min,max);
-  pf_ = edm::Service<DQMStore>()->bookProfile(std::string("fraction_"+rN),std::string("fraction of passing bits in run "+rN),nBin,min,max, 1000, 0.0, 1.0);
   
+
+  h1_ = edm::Service<DQMStore>()->book1D(std::string("passingBits_"+label_+"_"+rN),std::string("passing bits in run "+rN),nBin,min,max);
+  h2_ = edm::Service<DQMStore>()->book2D(std::string("passingBits_2_"+label_+"_"+rN),std::string("correlation between bits in run "+rN),nBin,min,max, nBin,min,max);
+  pf_ = edm::Service<DQMStore>()->bookProfile(std::string("fraction_"+label_+"_"+rN),std::string("fraction of passing bits in run "+rN),nBin,min,max, 1000, 0.0, 1.0);
+  if (denominator_!="")
+    ratio_ = edm::Service<DQMStore>()->bookProfile(std::string("ratio_"+label_+"_"+rN),std::string("fraction of passing bits in run "+rN+" with respect to: "+denominator_),nBin,min,max, 1000, 0.0, 1.0);
+  else 
+    ratio_=0;
+
   for (uint i=0; i!=nBin; ++i){
     h1_->getTH1F()->GetXaxis()->SetBinLabel(i+1,HLTPathsByName_[i].c_str());
     h2_->getTH2F()->GetXaxis()->SetBinLabel(i+1,HLTPathsByName_[i].c_str());
     h2_->getTH2F()->GetYaxis()->SetBinLabel(i+1,HLTPathsByName_[i].c_str());
     pf_->getTProfile()->GetXaxis()->SetBinLabel(i+1,HLTPathsByName_[i].c_str());
+    if (ratio_)
+      ratio_->getTProfile()->GetXaxis()->SetBinLabel(i+1,(HLTPathsByName_[i]+" & "+denominator_).c_str());
   }
 
 }
@@ -165,30 +177,19 @@ BitPlotting::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    for (unsigned int i=0; i!=n; i++) {
      HLTPathsByIndex_[i]=triggerNames_.triggerIndex(HLTPathsByName_[i]);
    }
-   
+   uint denominatorIndex = 0;
+   bool denominatorValidity= false;
+   if (denominator_!="") {
+     denominatorIndex=triggerNames_.triggerIndex(denominator_);
+     denominatorValidity= (denominatorIndex <trh->size());
+   }
 
-   std::vector<bool> validity;
-   validity.resize(n);
+   std::vector<bool> validity(n);
     
    for (unsigned int i=0; i!=n; i++) {
      validity[i]=( (HLTPathsByIndex_[i]<trh->size()) && (HLTPathsByName_[i]!=invalid) );
-     
-     /*
-     LogTrace("") << " " << HLTPathsByIndex_[i]
-		  << " " << HLTPathsByName_[i]
-		  << " " << validity;
-
-     if(!validity){
-       edm::LogError("BitPlotting")<<HLTPathsByName_[i]<<" is not in the HLT menu.";
-       return ;
-     }
-     */
-     //     if (!validity) throw cms::Exception("Configuration");
-       //       << " HLTHighLevel [instance: " << *moduleLabel()
-       //       << " - path: " << *pathName()
-       //       << "] configured with unknown HLT path name "
-       //       << i << " " << HLTPathsByName_[i] <<"\n";
    }
+
 
    std::stringstream report;
    std::string sep=" ";
@@ -204,7 +205,12 @@ BitPlotting::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	 //	 edm::LogError("BitPlotting")<<"filling: "<<i<<" to: "<< h1_->getName()<<std::endl;
 	 h1_->Fill(i);
 	 pf_->Fill(i,1);
-	 
+	 if (ratio_ && denominatorValidity){
+	   if (trh->accept(denominatorIndex))
+	     ratio_->Fill(i,1);
+	   else
+	     ratio_->Fill(i,0);
+	 }
 	 for (unsigned int j=0; j!=n; j++) {
 	   if (!validity[j]) continue;
 	   if (HLTPathsByIndex_[j]<trh->size()) {
@@ -257,7 +263,7 @@ BitPlotting::endJob() {
 
   edm::LogError("BitPlotting")<<report.str();
   total_=0;
-  if( out.size() != 0 ) edm::Service<DQMStore>()->save(out);
+  if( out_.size() != 0 ) edm::Service<DQMStore>()->save(out_);
 }
 
 //define this as a plug-in
