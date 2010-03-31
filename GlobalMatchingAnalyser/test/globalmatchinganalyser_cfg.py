7c884acac4fb805fc9d0ed9a663643eb99eaf178
aeverett <>
2010-03-31 04:28:07 +0000

    *** empty log message ***

--- GlobalMatchingAnalyser/test/globalmatchinganalyser_cfg.py
+++ GlobalMatchingAnalyser/test/globalmatchinganalyser_cfg.py
@@ -4,6 +4,7 @@ process = cms.Process("DebugMuon")
 # Messages
 ##process.load("RecoMuon.Configuration.MessageLogger_cfi")
 process.load("FWCore.MessageService.MessageLogger_cfi")
+
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.MessageLogger.destinations += ['AnalyzerMessages',
                                        'DetailedMessages',
@@ -15,7 +16,7 @@ process.MessageLogger.categories   += ['GlobalMuonTrajectoryBuilder',
                                        'GlobalTrajectoryBuilderBase',
                                        'GlobalMuonProducer',
                                        'MuonTrajectoryCleaner']
-process.MessageLogger.categories   += ['MuonTrackLoader','TrackFitters']
+process.MessageLogger.categories   += ['TrackFitters']
 process.MessageLogger.debugModules += ['globalMuons','globalMatchingAnalyser']
 
 
@@ -45,7 +46,6 @@ process.MessageLogger.FitterMessages  = cms.untracked.PSet(
     TrackFitters = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
     )
 
-
 # Muon Reco
 process.load("RecoLocalMuon.Configuration.RecoLocalMuon_cff")
 
@@ -66,92 +66,80 @@ process.load("Configuration.StandardSequences.Reconstruction_cff")
 
 process.source = cms.Source(
     "PoolSource",
-##     eventsToProcess = cms.untracked.VEventRange(
-## ##     '124020:19131248',
-## ##     '124020:28258367',
-## ##     '124022:20851004',
-## ##     '124022:28309645',
-## ##     '124023:13014532',
-## ##     '124023:21857034',
-## ##     '124230:15994834',
-## #
-##     '124020:5100126',
-##     '124020:28258367',
-##     '124022:13322446',
-##     '124022:28309645',
-##     '124023:10537672',
-##     '124024:5006249',
-##     '124027:10742619',
-##     '124230:11859102',
-##     '124230:18631437',
-    
-##     ),
+    # eventsToProcess = cms.untracked.VEventRange( '123977:11474609', ),
     fileNames = cms.untracked.vstring(
-    
-    # 'file:/scratch/scratch96/a/aeverett/FastAnalysis/split-mc-matched.root',
-
-    # 'file:/scratch/scratch96/a/aeverett/FastAnalysis/data/badGlb/fastReco-badGlobalMuons_10/myFastReco.root',
-    'file:/scratch/scratch96/a/aeverett/FastAnalysis/data/goodGlb/fastReco-goodGlobalMuons_21/myFastReco.root',
+    '/store/user/aeverett/MinimumBias//CMSSW_354_PUSkim_123978//679e85c2781449dc0ef0f390bc4dc871//muonSkim_1_1.root',
+    '/store/user/aeverett/MinimumBias//CMSSW_354_PUSkim_123818//679e85c2781449dc0ef0f390bc4dc871//muonSkim_1_1.root',
+    '/store/user/aeverett/MinimumBias//CMSSW_354_PUSkim_123976//679e85c2781449dc0ef0f390bc4dc871//muonSkim_1_1.root',
+    '/store/user/aeverett/MinimumBias//CMSSW_354_PUSkim_124030//679e85c2781449dc0ef0f390bc4dc871//muonSkim_1_1.root',
+    '/store/user/aeverett/MinimumBias//CMSSW_354_PUSkim_123591//679e85c2781449dc0ef0f390bc4dc871//muonSkim_1_1.root',
+    '/store/user/aeverett/MinimumBias//CMSSW_354_PUSkim_123592//679e85c2781449dc0ef0f390bc4dc871//muonSkim_1_1.root',
+    #'/store/user/aeverett/CMSSW_3_4_1/SingleMuPt0_500/aeverett/SingleMuPt0_500_CMSSW_3_4_1_step1//SingleMuPt0_500_CMSSW_3_4_1_step2//bd7cda0b32f61291da1aab637c754773//step2_23.root'
     
     )
     )
-
+    
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(2)
+    input = cms.untracked.int32(-1)
     )
 
-process.out = cms.OutputModule("PoolOutputModule",
-                               fileName = cms.untracked.string('myDebugMuonReco.zero.allBad.root')
-                               )
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string('myJustMuonReco.root')
+    )
 
 process.p = cms.Path(process.muonrecoComplete)
 #process.p = cms.Path(process.reconstruction)
 
-process.load("ISpy/Analyzers/ISpy_Producer_cff")
-process.add_(
-    cms.Service("ISpyService",
-    outputFileName = cms.untracked.string('myJustMuonRecoSpy.ig'),
-    outputMaxEvents = cms.untracked.int32(100),
-    )
-)
-process.p1 = cms.Path(process.iSpy_sequence)
+## process.load("ISpy/Analyzers/ISpy_Producer_cff")
+## process.add_(
+##     cms.Service("ISpyService",
+##     outputFileName = cms.untracked.string('myJustMuonRecoSpy.ig'),
+##     outputMaxEvents = cms.untracked.int32(100),
+##     )
+## )
+## process.p1 = cms.Path(process.iSpy_sequence)
 
 process.TFileService = cms.Service(
     "TFileService",
-    fileName = cms.string("TFS_allBad.root")
+    fileName = cms.string("TFS_debug.root")
     )
 
 process.load("UserCode.GlobalMatchingAnalyser.globalmatchinganalyser_cfi")
-
-process.globalMatchingAnalyser.GlobalMuonTrackMatcher.UseTFileService = cms.untracked.bool(True)
 
 process.analyser_step = cms.Path(process.globalMatchingAnalyser) 
 
 process.this_is_the_end = cms.EndPath(process.out)
 
+process.schedule = cms.Schedule(process.p,process.analyser_step,process.this_is_the_end)
+
 #
 # Additional tags and parameter changes
 #
 
-process.GlobalTag.globaltag = 'GR09_R_34X_V2::All'
-
-process.ttrhbwr.ComputeCoarseLocalPositionFromDisk = True
-
-process.globalMuons.TrackLoaderParameters.UseTFileService = cms.untracked.bool(True)
-process.standAloneMuons.TrackLoaderParameters.UseTFileService = cms.untracked.bool(True)
-process.globalMuons.GLBTrajBuilderParameters.GlobalMuonTrackMatcher.UseTFileService = cms.untracked.bool(True)
-process.globalMuons.GLBTrajBuilderParameters.UseTFileService = cms.untracked.bool(True)
-process.globalMuons.UseTFileService = cms.untracked.bool(True)
+process.GlobalTag.globaltag = 'GR10_P_V4::All'
 
 #process.globalMuons.GLBTrajBuilderParameters.PtCut = 0.5
 
 #process.globalMuons.GLBTrajBuilderParameters.GlobalMuonTrackMatcher.DeltaRCut_2 = 0.3
 #process.globalMatchingAnalyser.GlobalMuonTrackMatcher.DeltaRCut_2 = 0.3
 
+process.ttrhbwor.ComputeCoarseLocalPositionFromDisk = True
+process.ttrhbwr.ComputeCoarseLocalPositionFromDisk = True
+
+process.globalMatchingAnalyser.GlobalMuonTrackMatcher.UseTFileService = cms.untracked.bool(True)
+
+process.globalMuons.UseTFileService = cms.untracked.bool(True)
+process.globalMuons.TrackLoaderParameters.UseTFileService = cms.untracked.bool(True)
+process.globalMuons.GLBTrajBuilderParameters.UseTFileService = cms.untracked.bool(True)
+process.globalMuons.GLBTrajBuilderParameters.GlobalMuonTrackMatcher.UseTFileService = cms.untracked.bool(True)
+
+process.standAloneMuons.TrackLoaderParameters.UseTFileService = cms.untracked.bool(True)
+
 #process.globalMuons.MuonCollectionLabel = cms.InputTag("standAloneMuons","")
 
 def customise(process):
-    from Workspace.MuonRecoTreeUtility.muonRecoTreeUtility_cff import insertMRTU
+    from Workspace.MuonRecoTreeUtility.muonRecoTreeUtilityForData_cff import insertMRTU
     insertMRTU(process)
     return (process)
 
