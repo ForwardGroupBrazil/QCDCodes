4b1e0437dfdd50e090d19aafa0da1942441ae5d4
aeverett <>
2010-05-18 10:21:46 +0000

    add MEssageLogger

--- MuonRecoTreeUtility/test/MRTU_data_test.py
+++ MuonRecoTreeUtility/test/MRTU_data_test.py
@@ -7,6 +7,60 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('MRTU')
 
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+# Destinations
+process.MessageLogger.destinations += ['FitterMessages',
+                                       'DetailedMessages']
+
+# Categories
+process.MessageLogger.categories   += ['GlobalMuonTrackMatcher']
+
+process.MessageLogger.categories   += ['GlobalMuonTrajectoryBuilder',
+                                       'GlobalTrajectoryBuilderBase',
+                                       'GlobalMuonProducer',
+                                       'MuonTrajectoryCleaner']
+
+process.MessageLogger.categories   += ['MuonTrackLoader','TrackFitters','MuonRecoTreeUtility']
+
+# Modules to Debug.  Note that including globalMuons and
+# globalMatchingAnalyser will cause the matching printout to be repeated
+# since there is a call to the TrackMatcher within the matching
+# analyzer.
+process.MessageLogger.debugModules += ['globalMuons','recoMuonTreeMaker']
+
+# This one gives us some top level information about the STA, TK, GLB,
+# regional TK, matched TK, and the global refit.
+process.MessageLogger.DetailedMessages = cms.untracked.PSet(
+    threshold  = cms.untracked.string('DEBUG'),
+    default    = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+    INFO = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+    GlobalMuonTrackMatcher = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+    GlobalMuonTrajectoryBuilder = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+    GlobalTrajectoryBuilderBase = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+    GlobalMuonProducer = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+    MuonTrajectoryCleaner = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+
+    )
+
+# This one gives us the status of the track loader as well as the
+# possibility of a hit-by-hit status for the KFFitter and KFSmoother.
+# The KFFitter and KFSmoother stuff is only really useful if you suspect
+# a problem with the fit or the smooth.  Such a problem would manifest
+# itself, for example, in the DetailedMessages as a failure for the
+# sta-tk-pair to refit as a trajectory.
+process.MessageLogger.FitterMessages  = cms.untracked.PSet(
+     threshold  = cms.untracked.string('DEBUG'),
+     default    = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+     INFO = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+     MuonTrackLoader = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+     TrackFitters = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+     MuonRecoTreeUtility = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+     )
+
+
+
 # import of standard configurations
 process.load('Configuration/StandardSequences/Services_cff')
 process.load('FWCore/MessageService/MessageLogger_cfi')
@@ -22,7 +76,7 @@ process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration/EventContent/EventContent_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.151 $'),
+    version = cms.untracked.string('$Revision: 1.1 $'),
     annotation = cms.untracked.string('promptCollisionReco nevts:100'),
     name = cms.untracked.string('PyReleaseValidation')
 )
@@ -58,11 +112,17 @@ process.options = cms.untracked.PSet(
                                     'NotFound')    
     )
 # Input source
-process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring(
-    "/store/user/aeverett/aeverett//Commisioning//Data/PurdueSkim0307_01/muonSkim_123596_1.root",
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(
+    'file:/home/ba01/u112/aeverett/work/CMSSW_3_5_6/src/FastAnalysis/Skims/test/muonSkim.data.root'
+    ##     '/store/data/Commissioning10/MinimumBias/RAW-RECO/Apr1Skim_GOODCOLL-v1/0140/F4EA88D2-C83E-DF11-AF55-00261894392C.root',
+    ##     '/store/data/Commissioning10/MinimumBias/RAW-RECO/Apr1Skim_GOODCOLL-v1/0140/E27B88D1-8040-DF11-B3FC-00261894391B.root',
+    ##     '/store/data/Commissioning10/MinimumBias/RAW-RECO/Apr1Skim_GOODCOLL-v1/0140/E246ADD2-C83E-DF11-A9BE-00261894390C.root',
+    ##     '/store/data/Commissioning10/MinimumBias/RAW-RECO/Apr1Skim_GOODCOLL-v1/0140/E0F8D4D2-C83E-DF11-9249-002618943877.root',
+    ##     '/store/data/Commissioning10/MinimumBias/RAW-RECO/Apr1Skim_GOODCOLL-v1/0140/E0B003DD-C83E-DF11-8A5D-002618943947.root',
     ),
-                            )
+    )
 
 # Output definition
 process.output = cms.OutputModule("PoolOutputModule",
@@ -76,8 +136,9 @@ process.output = cms.OutputModule("PoolOutputModule",
 )
 
 # Other statements
-process.GlobalTag.globaltag = 'GR09_R_34X_V2::All'
+process.GlobalTag.globaltag = 'GR_R_35X_V6::All'
 process.ttrhbwr.ComputeCoarseLocalPositionFromDisk = True
+process.ttrhbwor.ComputeCoarseLocalPositionFromDisk = True
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
