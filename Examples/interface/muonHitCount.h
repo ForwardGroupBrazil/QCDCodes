5ebcc7cb9333f726f4760b06c92e6daf79482e3d
aeverett <>
2010-06-06 17:26:03 +0000

    initial commit

---
+++ Examples/interface/muonHitCount.h
@@ -0,0 +1,13 @@
+#ifndef UserCode_Examples_interface_muonHitCount_h
+#define UserCode_Examples_interface_muonHitCount_h
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
+namespace muon {
+    /** Count the number of muon stations from a given track
+        subdet = 0 for all subdetectors, or 1/2/3 for DT/CSC/RPC (as defined in MuonSubdetId 
+        validOnly = true (default) to include only valid hits, = false to include also bad ones 
+        note: requires the TrackExtra and the TrackingRecHits to be available.
+      */
+    int muonHitCount(const reco::TrackRef track, int subdet=0, bool validOnly=true) ;
+}
+#endif
