e593f566f4483033291c11e4ab5e0dd9534fba0b
kkousour <>
2011-06-22 09:59:36 +0000

    Script to merge the mc files

---
+++ QCDAnalysis/test/MergeFiles_mc.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+import os
+import getopt
+import sys
+
+path        = "/pnfs/cms/WAX/11/store/user/kkousour/2011/Jets/mc/"
+prefix      = "dcap://cmsdca1.fnal.gov:24140/pnfs/fnal.gov/usr/cms/WAX/11/store/user/kkousour/2011/Jets/mc/"
+destination = "/uscms_data/d2/kkousour/7TeV/2011/Jets/mc/"
+
+DIR = os.listdir(path)
+
+ss = "InclusiveJetsTree_mc"
+
+for dd in DIR:
+  print "Reading directory "+dd
+  ROOTFILES = os.listdir(path+"/"+dd)
+  command = "hadd -f "+destination+"/"+dd+"_"+ss+".root "
+  for ll in ROOTFILES:
+    command += prefix+dd+"/"+ll+" "
+  print command
+  os.system(command)
