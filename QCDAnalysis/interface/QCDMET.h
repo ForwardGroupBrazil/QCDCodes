52806304880509433d19c4cb352019c836b41c61
kkousour <>
2011-06-22 09:53:23 +0000

    Added the MET phi

--- QCDAnalysis/interface/QCDMET.h
+++ QCDAnalysis/interface/QCDMET.h
@@ -8,13 +8,15 @@ class QCDMET
      QCDMET() {et_=0; sumEt_=0;}
      //------------ Destructor -------------------------------
      ~QCDMET() {}
-     void setVar(float fEt, float fSumEt) {et_ = fEt;sumEt_=fSumEt;} 
+     void setVar(float fEt, float fSumEt, float fPhi) {et_ = fEt; sumEt_ = fSumEt; phi_ = fPhi;} 
      float met()         const {return et_;}
+     float phi()         const {return phi_;}
      float sumet()       const {return sumEt_;}
      float met_o_sumet() const {return et_/sumEt_;}
      
    private:
      float et_;
      float sumEt_;
+     float phi_;
 };
 #endif
