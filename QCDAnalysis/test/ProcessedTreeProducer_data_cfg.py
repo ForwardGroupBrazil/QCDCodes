c5cfc033b3dd653c53a6e3860b90232765b2c0f8
kkousour <>
2011-07-09 07:52:49 +0000

    Update to the EPS sample

--- QCDAnalysis/test/ProcessedTreeProducer_data_cfg.py
+++ QCDAnalysis/test/ProcessedTreeProducer_data_cfg.py
@@ -5,45 +5,45 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 ##-------------------- Communicate with the DB -----------------------
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'GR_R_42_V12::All'
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-from CondCore.DBCommon.CondDBSetup_cfi import *
-process.jec = cms.ESSource("PoolDBESSource",
-      DBParameters = cms.PSet(
-        messageLevel = cms.untracked.int32(0)
-        ),
-      timetype = cms.string('runnumber'),
-      toGet = cms.VPSet(
-      cms.PSet(
-            record = cms.string('JetCorrectionsRecord'),
-            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK5PF'),
-            label  = cms.untracked.string('AK5PF')
-            ),
-      cms.PSet(
-            record = cms.string('JetCorrectionsRecord'),
-            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK7PF'),
-            label  = cms.untracked.string('AK7PF')
-            ),
-      cms.PSet(
-            record = cms.string('JetCorrectionsRecord'),
-            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK5Calo'),
-            label  = cms.untracked.string('AK5Calo')
-            ),
-      cms.PSet(
-            record = cms.string('JetCorrectionsRecord'),
-            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK7Calo'),
-            label  = cms.untracked.string('AK7Calo')
-            )
-      ), 
-      connect = cms.string('sqlite:Jec11V2.db')
-)
+process.GlobalTag.globaltag = 'GR_R_42_V19::All'
+#process.load("CondCore.DBCommon.CondDBCommon_cfi")
+#from CondCore.DBCommon.CondDBSetup_cfi import *
+#process.jec = cms.ESSource("PoolDBESSource",
+#      DBParameters = cms.PSet(
+#        messageLevel = cms.untracked.int32(0)
+#        ),
+#      timetype = cms.string('runnumber'),
+#      toGet = cms.VPSet(
+#      cms.PSet(
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK5PF'),
+#            label  = cms.untracked.string('AK5PF')
+#            ),
+#      cms.PSet(
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK7PF'),
+#            label  = cms.untracked.string('AK7PF')
+#            ),
+#      cms.PSet(
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK5Calo'),
+#            label  = cms.untracked.string('AK5Calo')
+#            ),
+#      cms.PSet(
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_Jec11V2_AK7Calo'),
+#            label  = cms.untracked.string('AK7Calo')
+#            )
+#      ), 
+#      connect = cms.string('sqlite:Jec11V2.db')
+#)
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.Geometry_cff')
 process.load('RecoJets.Configuration.RecoPFJets_cff')
 process.load('RecoJets.Configuration.RecoJets_cff')
 ##-------------------- Import the JEC services -----------------------
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+#process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
 #############   Set the number of events #############
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
@@ -52,10 +52,9 @@ process.maxEvents = cms.untracked.PSet(
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 #############   Define the source file ###############
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/data/Run2011A/Jet/AOD/PromptReco-v4/000/166/530/B6A20FDB-4C92-E011-89C2-0030487C6090.root',
-        '/store/data/Run2011A/Jet/AOD/PromptReco-v4/000/166/514/F4DC9A2D-1092-E011-BD53-0019B9F72D71.root',
-        '/store/data/Run2011A/Jet/AOD/PromptReco-v4/000/166/514/DE97FB28-2392-E011-9082-003048F118AC.root',
-        '/store/data/Run2011A/Jet/AOD/PromptReco-v4/000/166/514/C66FA822-1C92-E011-9735-0030487CD178.root')
+    fileNames = cms.untracked.vstring(
+       '/store/data/Run2011A/Jet/AOD/PromptReco-v4/000/167/913/EEE2D986-8EA3-E011-8995-003048F118AC.root'
+    )
 )
 ############# processed tree producer ##################
 process.TFileService = cms.Service("TFileService",fileName = cms.string('ProcessedTree_data.root'))
@@ -79,25 +78,25 @@ process.ak7 = cms.EDAnalyzer('ProcessedTreeProducer',
     srcPFRho        = cms.InputTag('kt6PFJets','rho'),
     ## preselection cuts #########################
     maxY            = cms.double(5.0), 
-    minPFPt         = cms.double(50),
+    minPFPt         = cms.double(20),
     minPFFatPt      = cms.double(10),
     maxPFFatEta     = cms.double(2.5),
-    minCaloPt       = cms.double(50),
+    minCaloPt       = cms.double(20),
     minNPFJets      = cms.int32(1),
     minNCaloJets    = cms.int32(1), 
     minJJMass       = cms.double(-1),
     ## trigger ###################################
     printTriggerMenu = cms.untracked.bool(True),
     processName     = cms.string('HLT'),
-    triggerName     = cms.vstring('HLT_Jet30_v1','HLT_Jet30_v2','HLT_Jet30_v3','HLT_Jet30_v4',
-                                  'HLT_Jet60_v1','HLT_Jet60_v2','HLT_Jet60_v3','HLT_Jet60_v4',
-                                  'HLT_Jet80_v1','HLT_Jet80_v2','HLT_Jet80_v3','HLT_Jet80_v4',
-                                  'HLT_Jet110_v1','HLT_Jet110_v2','HLT_Jet110_v3','HLT_Jet110_v4',
-                                  'HLT_Jet150_v1','HLT_Jet150_v2','HLT_Jet150_v3','HLT_Jet150_v4',
-                                  'HLT_Jet190_v1','HLT_Jet190_v2','HLT_Jet190_v3','HLT_Jet190_v4',
-                                  'HLT_Jet240_v1','HLT_Jet240_v2','HLT_Jet240_v3','HLT_Jet240_v4',
-                                  'HLT_Jet300_v1', 'HLT_Jet300_v2','HLT_Jet300_v3',
-                                  'HLT_Jet370_v1','HLT_Jet370_v2','HLT_Jet370_v3','HLT_Jet370_v4'),
+    triggerName     = cms.vstring('HLT_Jet30_v1','HLT_Jet30_v2','HLT_Jet30_v3','HLT_Jet30_v4','HLT_Jet30_v5','HLT_Jet30_v6',
+                                  'HLT_Jet60_v1','HLT_Jet60_v2','HLT_Jet60_v3','HLT_Jet60_v4','HLT_Jet60_v5','HLT_Jet60_v6',
+                                  'HLT_Jet80_v1','HLT_Jet80_v2','HLT_Jet80_v3','HLT_Jet80_v4','HLT_Jet80_v5','HLT_Jet80_v6',
+                                  'HLT_Jet110_v1','HLT_Jet110_v2','HLT_Jet110_v3','HLT_Jet110_v4','HLT_Jet110_v5','HLT_Jet110_v6',
+                                  'HLT_Jet150_v1','HLT_Jet150_v2','HLT_Jet150_v3','HLT_Jet150_v4','HLT_Jet150_v5','HLT_Jet150_v6',
+                                  'HLT_Jet190_v1','HLT_Jet190_v2','HLT_Jet190_v3','HLT_Jet190_v4','HLT_Jet190_v5','HLT_Jet190_v6',
+                                  'HLT_Jet240_v1','HLT_Jet240_v2','HLT_Jet240_v3','HLT_Jet240_v4','HLT_Jet240_v5','HLT_Jet240_v6',
+                                  'HLT_Jet300_v1', 'HLT_Jet300_v2','HLT_Jet300_v3','HLT_Jet300_v4','HLT_Jet300_v5',
+                                  'HLT_Jet370_v1','HLT_Jet370_v2','HLT_Jet370_v3','HLT_Jet370_v4','HLT_Jet370_v5','HLT_Jet370_v6'),
     triggerResults  = cms.InputTag("TriggerResults","","HLT"),
     triggerEvent    = cms.InputTag("hltTriggerSummaryAOD","","HLT"),
     ## jec services ##############################
--- QCDAnalysis/test/TransferFiles.py
+++ QCDAnalysis/test/TransferFiles.py
@@ -1,31 +1,37 @@
 #! /usr/bin/env python
 import os
 
+#setenv LCG_GFAL_INFOSYS lcg-bdii.cern.ch
+
 #-------------------- DATA ------------------------------
-location    = "/uscms_data/d2/kkousour/7TeV/2011/Jets/data/June21st/"
+location    = "/uscms_data/d2/kkousour/7TeV/2011/Jets/data/July8th/"
 filename    = "InclusiveJetsTree_data.root"
 
 print "Transfer DATA file to FNAL:"
-destination = "/11/store/user/kkousour/2011/Jets/production/June21st/"
+destination = "/11/store/user/kkousour/2011/Jets/production/July8th/"
 command = "srmcp \"file:///"+location+filename+"\" "+"\"srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN="+destination+filename+"\""
 print command
+os.system(command)
 
 print "Transfer DATA file to CERN:"
-destination = "/castor/cern.ch/user/k/kkousour/7TeV/data/Jets/Inclusive/June21st/"
-command = "srmcp \"file:///"+location+filename+"\" "+"\"srm://srm-cms.cern.ch:8443/srm/managerv2?SFN="+destination+filename+"\""
+destination = "/castor/cern.ch/user/k/kkousour/7TeV/data/Jets/Inclusive/July8th/"
+command = "lcg-cp -v -n 10 \"file:///"+location+filename+"\" "+"\"srm://srm-cms.cern.ch:8443/srm/managerv2?SFN="+destination+filename+"\""
 print command
+os.system(command)
 
 #-------------------- MC-- ------------------------------
-location    = "/uscms_data/d2/kkousour/7TeV/2011/Jets/mc/"
-filename    = "InclusiveJetsTree_mc.root"
+#location    = "/uscms_data/d2/kkousour/7TeV/2011/Jets/mc/"
+#filename    = "Summer11Herwig23Flat_InclusiveJetsTree_mc.root"
 
-print "Transfer MC file to FNAL:"
-destination = "/11/store/user/kkousour/2011/Jets/production/mc/"
-command = "srmcp \"file:///"+location+filename+"\" "+"\"srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN="+destination+filename+"\""
-print command
+#print "Transfer MC file to FNAL:"
+#destination = "/11/store/user/kkousour/2011/Jets/production/mc/"
+#command = "srmcp \"file:///"+location+filename+"\" "+"\"srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN="+destination+filename+"\""
+#print command
+#os.system(command)
 
-print "Transfer MC file to CERN:"
-destination = "/castor/cern.ch/user/k/kkousour/7TeV/data/Jets/Inclusive/mc/"
-command = "srmcp \"file:///"+location+filename+"\" "+"\"srm://srm-cms.cern.ch:8443/srm/managerv2?SFN="+destination+filename+"\""
-print command
+#print "Transfer MC file to CERN:"
+#destination = "/castor/cern.ch/user/k/kkousour/7TeV/data/Jets/Inclusive/mc/"
+#command = "lcg-cp -v -n 10 \"file:///"+location+filename+"\" "+"\"srm://srm-cms.cern.ch:8443/srm/managerv2?SFN="+destination+filename+"\""
+#print command
+#os.system(command)
 
