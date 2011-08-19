b51f72568cdd7f667d8c528cb9bebd916e1d56a2
kkousour <>
2011-08-19 09:34:37 +0000

    *** empty log message ***

--- QCDAnalysis/test/ReadTree.C
+++ QCDAnalysis/test/ReadTree.C
@@ -1,35 +1,135 @@
 void ReadTree()
 {
-  gROOT->ProcessLine("#include <vector>");
+  //------------ FWLite libraries ------
   gSystem->Load("libFWCoreFWLite.so");
   AutoLibraryLoader::enable();
-  TFile *inf  = TFile::Open("ProcessedTree_data.root");
+  //------------ files -----------------
+  TFile *inf  = TFile::Open("/uscms_data/d2/kkousour/7TeV/2011/Jets/data/July8th/InclusiveJetsTree_data.root");
+  TFile *outf = new TFile("DemoHistos.root","RECREATE");
   TTree *tr = (TTree*)inf->Get("ak7/ProcessedTree");
+  //----------- define the tree branch -
   QCDEvent *Event = new QCDEvent();
   TBranch *branch = tr->GetBranch("events");
   branch->SetAddress(&Event);
-  
+  //----------- settings ---------------
+  int NEVENTS(10000);
+  TString HLT("HLT_Jet370_v3");
+  double PTMIN(100);
+  //--------- book histos ---------------------------------
+  TH1F *hCorPt = new TH1F("CorrectedJetPt","CorrectedJetPt",350,0,3500);
+  TH1F *hY     = new TH1F("JetRapidity","JetRapidity",100,-5,5);
+  TH1F *hMET   = new TH1F("METoverSUMET","METoverSUMET",100,0,1.0001);
+  TH1F *hNPV   = new TH1F("NumberOfVertices","NumberOfVertices",30,0,30);
+  TH1F *hRho   = new TH1F("PtDensityRho","PtDensityRho",50,0,50);
+  //--------- trigger mapping ---------------------------------
+  //--------- read the histogram with the trigger names -------
+  TH1F *hTrigNames = (TH1F*)inf->Get("ak7/TriggerNames");
+  int ihlt(-1);
+  cout<<"Finding trigger mapping: "<<endl;
+  //----------- loop over the X-axis labels -----------------
+  for(int ibin=0;ibin<hTrigNames->GetNbinsX();ibin++) {
+    TString ss(hTrigNames->GetXaxis()->GetBinLabel(ibin+1));
+    if (ss == HLT) {
+      ihlt = ibin;
+      continue;
+    }
+  }
+  if (ihlt == -1) {
+    cout<<"The requested trigger ("<<HLT<<") is not found ";
+    break;
+  }
+  else {
+    cout<<HLT<<" --> "<<ihlt<<endl;
+  }
+  //----------- counters -----------------------
+  int counter_hlt(0),counter_pv(0),counter_hcal(0),counter_jet(0);
+  //----------- tree reading -------------------
   unsigned NEntries = tr->GetEntries();
   cout<<"Reading TREE: "<<NEntries<<" events"<<endl;
-  int    decade = 0;
-  
-  for(unsigned i=0;i<NEntries;i++) {
-    double progress = 10.0*i/(1.0*NEntries);
+  int decade = 0;
+  for(unsigned i=0;i<NEVENTS;i++) {
+    //----------- progress report -------------
+    double progress = 10.0*i/(1.0*NEVENTS);
     int k = TMath::FloorNint(progress); 
     if (k > decade) 
       cout<<10*k<<" %"<<endl;
     decade = k;          
+    //----------- read the event --------------
     tr->GetEntry(i);
-    for(unsigned itrig=0;itrig<Event->nTriggers();itrig++) {
-      cout<<"Trigger #"<<itrig<<", decision = "<<Event->fired(itrig)<<endl;
-      if (Event->fired(itrig) > 0) {
-        cout<<"Found "<<Event->nL1Obj(itrig)<<" L1 objects: "<<endl;
-        for(unsigned iobj=0;iobj<Event->nL1Obj(itrig);iobj++)
-          cout<<"pt = "<<Event->l1obj(itrig,iobj).pt()<<endl;
-        cout<<"Found "<<Event->nHLTObj(itrig)<<" HLT objects: "<<endl;
-        for(unsigned iobj=0;iobj<Event->nHLTObj(itrig);iobj++)
-          cout<<"pt = "<<Event->hltobj(itrig,iobj).pt()<<endl;
+    //----------- check if the trigger was fired ---
+    bool hltPass(false);
+    int prescale(1);
+    if (ihlt == -1)
+      hltPass = true; // no trigger set
+    else {
+      if (Event->fired(ihlt) > 0) {
+        hltPass = true;
+        prescale = Event->preL1(ihlt) * Event->preHLT(ihlt);
       }
     }
-  }
+    if (hltPass) {
+      counter_hlt++;
+      //-------- check if the primary vertex is good ----
+      if (Event->evtHdr().isPVgood() == 1) {
+        counter_pv++; 
+        //-------- check the loose HCAL noise filter ----------
+        if (Event->evtHdr().looseHCALNoise()) {
+          counter_hcal++;
+          //------- fill the MET/SumET control histo ----------
+          hMET->Fill(Event->pfmet().met_o_sumet());
+          //------- fill the NPV histo ------------------------
+          hNPV->Fill(Event->evtHdr().nVtxGood());          
+          //------- fill the Rho histo ------------------------
+          hRho->Fill(Event->evtHdr().pfRho());
+          //------- loop over the PF jets ---------------------
+          for(int j=0;j<Event->nPFJets();j++) { 
+            //----- apply the pt and ID cuts ------------------
+            if (Event->pfjet(j).ptCor() >= PTMIN && Event->pfjet(j).tightID()) {
+              counter_jet++;
+              //------- fill some histograms ------------------
+              hCorPt->Fill(Event->pfjet(j).ptCor());
+              hY->Fill(Event->pfjet(j).y()); 
+            }  
+          }// jet loop
+        }// hcal noise filter    
+      }// pv cut
+    }// hlt
+  }// tree loop
+  //----------------- print out some information ---------------
+  cout<<"Events read:                      "<<NEVENTS<<endl;
+  cout<<"Events after the trigger cut:     "<<counter_hlt<<endl;
+  cout<<"Events after the PV cut:          "<<counter_pv<<endl;
+  cout<<"Events after the HCAL filter cut: "<<counter_hcal<<endl;
+  cout<<"Number of jets:                   "<<counter_jet<<endl;  
+  //----------------- save the histos to the output file -------
+  outf->Write();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
