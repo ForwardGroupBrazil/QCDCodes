6dc14bcfcc7379489a700e68c153bf80c8c44a67
kkousour <>
2011-08-26 07:49:46 +0000

    *** empty log message ***

--- QCDAnalysis/test/InclusiveHistos_data_cfg.py
+++ QCDAnalysis/test/InclusiveHistos_data_cfg.py
@@ -11,10 +11,9 @@ process.source = cms.Source("EmptySource")
 
 ##-------------------- User analyzer  --------------------------------
 process.inclusive    = cms.EDAnalyzer('InclusiveHistos',
-    filename         = cms.string('/uscms_data/d2/kkousour/7TeV/2011/Jets/data/July8th/InclusiveJetsTree_data.root'),
+    filename         = cms.string('/uscms_data/d2/kkousour/7TeV/2011/Jets/data/Aug22nd/InclusiveJetsTree_data.root'),
     treename         = cms.string('ProcessedTree'),
     dirname          = cms.string('ak7'),
-    #pfYBiasCor       = cms.untracked.string('YBiasCorrection.txt'),
     yBnd             = cms.vdouble(0.0,0.5,1.0,1.5,2.0,2.5,3.0),
     ptBnd            = cms.vdouble(40,53,67,81,97,114,133,153,174,196,220,245,272,300,330,362,395,430,468,507,548,592,638,686,737,790,846,905,967,1032,1101,1172,1248,1327,1410,1497,1588,1684,1784,1890,2000,2116,2238,2366,2500,2640,2787,2941,3103,3273,3500),
     triggers         = cms.vstring('HLT_Jet30_v1','HLT_Jet30_v2','HLT_Jet30_v3','HLT_Jet30_v4','HLT_Jet30_v5','HLT_Jet30_v6',
--- QCDAnalysis/test/MultijetSearchHistos_cfg.py
+++ QCDAnalysis/test/MultijetSearchHistos_cfg.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("myprocess")
-process.TFileService=cms.Service("TFileService",fileName=cms.string('MultijetSearchHistos_superTight_HT450.root'))
+process.TFileService=cms.Service("TFileService",fileName=cms.string('MultijetSearchHistos_8j_HT550_Pt50.root'))
 
 ##-------------------- Define the source  ----------------------------
 process.maxEvents = cms.untracked.PSet(
@@ -11,13 +11,14 @@ process.source = cms.Source("EmptySource")
 
 ##-------------------- User analyzer  --------------------------------
 process.multijet    = cms.EDAnalyzer('MultijetSearchHistos',
-    filename        = cms.string('/uscms_data/d2/kkousour/7TeV/2011/Jets/data/July8th/InclusiveJetsTree_HT_data.root'),
+    filename        = cms.string('/uscms_data/d2/kkousour/7TeV/2011/Jets/data/Aug22nd/MultiJetsTree_data.root'),
     treename        = cms.string('ProcessedTree'),
     dirname         = cms.string('ak5'),
-    triggers        = cms.vstring('HLT_HT450_v1','HLT_HT450_v2','HLT_HT450_v3','HLT_HT450_v4',
-                                  'HLT_HT450_v5','HLT_HT450_v6','HLT_HT450_v7'),
-    minPt           = cms.vdouble(160,120,90,70,50,40,30,30),
+    triggers        = cms.vstring('HLT_HT550_v1','HLT_HT550_v2','HLT_HT550_v3','HLT_HT550_v4',
+                                  'HLT_HT550_v5','HLT_HT550_v6','HLT_HT550_v7','HLT_HT550_v8'),
+    minPt           = cms.vdouble(50,50,50,50,50,50,50,50),
     maxEta          = cms.double(2.5),
+    minHT           = cms.double(650),
     rank            = cms.int32(8),
     jetID           = cms.int32(3),
     hcalNoiseFilter = cms.int32(1),
