e25e5b23b680bb603132ff381a8938c5f737bac8
dfigueiredo <>
2012-03-21 11:22:35 +0000

    This commit was manufactured by cvs2svn to create tag 'V01-00-00'.
    Sprout from master 2012-03-21 11:22:34 UTC kkousour '*** empty log message ***'
    Cherrypick from master 2012-02-21 09:26:38 UTC kkousour '*** empty log message ***':
        MultiJetAnalysis/test/EightJets/flatTree_mc_cfg.py
        MultiJetAnalysis/test/EightJets/patTuple_mc_cfg.py
    Delete:
        data/Jec10V3.db
        data/Jec11V0.db
        data/Jec11V0testL1Hybrid.db
        data/Jec11V1.db
        data/Jec11V2.db

--- MultiJetAnalysis/test/EightJets/flatTree_mc_cfg.py
+++ MultiJetAnalysis/test/EightJets/flatTree_mc_cfg.py
@@ -7,22 +7,21 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.TFileService=cms.Service("TFileService",fileName=cms.string('flatTree.root'))
 
 ##-------------------- Define the source  ----------------------------
-process.source = cms.Source("PoolSource",
-        fileNames = cms.untracked.vstring(
-        'file://./patTuple.root'
-        )
-)
-#process.load('KKousour.MultiJetAnalysis.QCD_HT-1000_cfi')
-#process.maxEvents = cms.untracked.PSet(
-#        input = cms.untracked.int32(-1)
+#process.source = cms.Source("PoolSource",
+#        fileNames = cms.untracked.vstring(
+#        'file://./patuple_madgraph.root'
 #        )
+#)
+process.load('KKousour.MultiJetAnalysis.QCD_HT-1000_cfi')
+process.maxEvents = cms.untracked.PSet(
+        input = cms.untracked.int32(-1)
+        )
 #############   Format MessageLogger #################
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 ##-------------------- User analyzer  --------------------------------
 process.multijets = cms.EDAnalyzer('PatMultijetSearchTree',
-    jets    = cms.InputTag('jetExtender','extendedPatJets'),
+    jets    = cms.InputTag('selectedPatJets'),
     met     = cms.InputTag('pfMet'),
-    rho     = cms.InputTag('kt6PFJets','rho'),
     beta    = cms.string('betaAK5PF'),
     etaMAX  = cms.double(2.5),
     ptMIN   = cms.double(30),
